6017eb34fd578ffb9c2fdbcef68875e2bb3986a7
justinguto <jutogustowski@gmail.com>
2026-05-10 12:03:05 -0700

    Complete A72 fold functions

--- main.hpp
+++ main.hpp
@@ -11,16 +11,40 @@ void printout(int[], int);
 void swapfold(int[], int);
 void sumfold(int[], int);
 void equalfold(int[], int[], int);
+
 const int SIZE = 100;
 
 void swapfold(int number[], int last)
 {
+    for (int i = 0; i < last / 2; i++)
+    {
+        int temp = number[i];
+        number[i] = number[last - i - 1];
+        number[last - i - 1] = temp;
+    }
 }
+
 void sumfold(int number[], int last)
 {
+    int folded = (last + 1) / 2;
+
+    for (int i = 0; i < folded; i++)
+    {
+        number[i] = number[i] + number[last - i - 1];
+    }
 }
+
 void equalfold(int number[], int equal[], int last)
 {
+    int folded = last / 2;
+
+    for (int i = 0; i < folded; i++)
+    {
+        if (number[i] == number[last - i - 1])
+            equal[i] = 1;
+        else
+            equal[i] = 0;
+    }
 }
 
 /***************************************************
@@ -28,20 +52,25 @@ void equalfold(int number[], int equal[], int last)
  ***************************************************/
 int makearray(int number[])
 {
-	int last;
-	srand(time(NULL));
-	do
-	{
-		last = rand() % 20;
-	} while (last < 10);
+    int last;
+    srand(time(NULL));
 
-	for (int i = 0; i < last; i++)
-		number[i] = rand() % 10;
-	return last;
+    do
+    {
+        last = rand() % 20;
+    }
+    while (last < 10);
+
+    for (int i = 0; i < last; i++)
+        number[i] = rand() % 10;
+
+    return last;
 }
+
 void printout(int number[], int last)
 {
-	for (int i = 0; i < last; i++)
-		cout << setw(5) << number[i];
-	cout << endl;
+    for (int i = 0; i < last; i++)
+        cout << setw(5) << number[i];
+
+    cout << endl;
 }
